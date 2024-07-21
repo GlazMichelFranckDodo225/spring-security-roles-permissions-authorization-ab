9c7898f03edb898a06281cecda97b8f46af48f81
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-21 10:10:56 +0200

    Add Admin and Management Controllers

---
+++ src/main/java/com/dgmf/controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.dgmf.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+    @GetMapping
+    public String get() {
+        return "GET::AdminController";
+    }
+
+    @PostMapping
+    public String post() {
+        return "POST::AdminController";
+    }
+
+    @PutMapping
+    public String put() {
+        return "PUT::AdminController";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "DELETE::AdminController";
+    }
+}
---
+++ src/main/java/com/dgmf/controller/ManagementController.java
@@ -0,0 +1,27 @@
+package com.dgmf.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/management")
+public class ManagementController {
+    @GetMapping
+    public String get() {
+        return "GET::ManagementController";
+    }
+
+    @PostMapping
+    public String post() {
+        return "POST::ManagementController";
+    }
+
+    @PutMapping
+    public String put() {
+        return "PUT::ManagementController";
+    }
+    
+    @DeleteMapping
+    public String delete() {
+        return "DELETE::AdminController";
+    }
+}
